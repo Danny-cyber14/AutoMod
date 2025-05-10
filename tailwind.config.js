0ae392634ebe155067a4403c63d650d879261588
Pender Daniel <danielefepender@gmail.com>
2025-05-10 13:10:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./dist/**/*.{html,js}", "./dist/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish: ['Mulish', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
